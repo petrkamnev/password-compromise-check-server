38d3cb573d05dad1969565c76e433ca8b526b50e
Petr Kamnev <petrkamnevyt@gmail.com>
2024-04-22 16:01:00 +0300

    psi ntlm support

--- cmd/pccserver/server.go
+++ cmd/pccserver/server.go
@@ -149,104 +149,110 @@ func handlePwnedPassword(w http.ResponseWriter, r *http.Request) {
 func handlePSI(w http.ResponseWriter, r *http.Request) {
 	prefix := strings.ToUpper(strings.TrimPrefix(r.URL.Path, "/psi/"))
 	mode := r.URL.Query().Get("mode")
-	if mode != "ntlm" {
-		// Construct the filename based on the given prefix
-		filename := getStoragePath() + prefix + ".txt"
+	var folderPath string
 
-		// Read the request body
-		requestBody, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Error reading request body", http.StatusInternalServerError)
-			return
-		}
-
-		psiRequest := &psi_proto.Request{}
-		err = proto.Unmarshal(requestBody, psiRequest)
-		if err != nil {
-			fmt.Errorf("Failed to deserialize request: %v", err)
-		}
-
-		// Check if the file exists
-		_, err = os.Stat(filename)
-		if os.IsNotExist(err) {
-			// If the file doesn't exist, set the response code to 400 and write the error message to the response body
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("The hash prefix was not in a valid format"))
-			return
-		}
-
-		// Open the file
-		file, err := os.Open(filename)
-		if err != nil {
-			// If there is an error opening the file, handle it (you may choose to log the error or handle it differently)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal Server Error"))
-			return
-		}
-		defer file.Close()
-
-		// Get file information
-		fileInfo, err := file.Stat()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal Server Error"))
-			return
-		}
-
-		fileSize := fileInfo.Size()
-		fileContent := make([]byte, fileSize)
-		_, err = file.Read(fileContent)
-		if err != nil {
-			// If there is an error reading the file, handle it (you may choose to log the error or handle it differently)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal Server Error"))
-			return
-		}
-
-		// Split file content into lines
-		lines := strings.Split(string(fileContent), "\n")
-
-		// Extract values from lines
-		var values []string
-		for _, line := range lines {
-			parts := strings.Split(line, ":")
-			if len(parts) == 2 {
-				values = append(values, strings.TrimSpace(parts[0]))
-			}
-		}
-
-		server, err := psi_server.CreateWithNewKey(true)
-		if err != nil {
-			fmt.Errorf("Failed to create a PSI server %v", err)
-		}
-
-		// Create the setup
-		serverSetup, err := server.CreateSetupMessage(0, 1, values, psi_ds.Raw)
-		if err != nil {
-			fmt.Errorf("Failed to create serverSetup: %v", err)
-		}
-		serializedServerSetup, err := proto.Marshal(serverSetup)
-		if err != nil {
-			fmt.Errorf("Failed to serialize serverSetup: %v", err)
-		}
-
-		// Get the response
-		response, err := server.ProcessRequest(psiRequest)
-		if err != nil {
-			fmt.Errorf("Failed to process request: %v", err)
-		}
-		serializedResponse, err := proto.Marshal(response)
-		if err != nil {
-			fmt.Errorf("Failed to serialize response: %v", err)
-		}
-
-		// Send the serialized response back to the client
-		w.Header().Set("Content-Type", "application/octet-stream")
-		w.Header().Set("PSI-Response-Length", fmt.Sprint(len(serializedResponse)))
-		w.Header().Set("PSI-Setup-Length", fmt.Sprint(len(serializedServerSetup)))
-		w.Write(serializedResponse)
-		w.Write(serializedServerSetup)
-		// Set the response code to 200
-		w.WriteHeader(http.StatusOK)
+	// Determine the storage subdirectory based on the mode parameter
+	if mode == "ntlm" {
+		folderPath = "ntlm"
+	} else {
+		folderPath = "sha1"
 	}
+
+	// Construct the filename based on the given prefix
+	filename := filepath.Join(getStoragePath(), folderPath, prefix+".txt")
+
+	// Read the request body
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	psiRequest := &psi_proto.Request{}
+	err = proto.Unmarshal(requestBody, psiRequest)
+	if err != nil {
+		fmt.Errorf("Failed to deserialize request: %v", err)
+	}
+
+	// Check if the file exists
+	_, err = os.Stat(filename)
+	if os.IsNotExist(err) {
+		// If the file doesn't exist, set the response code to 400 and write the error message to the response body
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("The hash prefix was not in a valid format"))
+		return
+	}
+
+	// Open the file
+	file, err := os.Open(filename)
+	if err != nil {
+		// If there is an error opening the file, handle it (you may choose to log the error or handle it differently)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+	defer file.Close()
+
+	// Get file information
+	fileInfo, err := file.Stat()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+
+	fileSize := fileInfo.Size()
+	fileContent := make([]byte, fileSize)
+	_, err = file.Read(fileContent)
+	if err != nil {
+		// If there is an error reading the file, handle it (you may choose to log the error or handle it differently)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+
+	// Split file content into lines
+	lines := strings.Split(string(fileContent), "\n")
+
+	// Extract values from lines
+	var values []string
+	for _, line := range lines {
+		parts := strings.Split(line, ":")
+		if len(parts) == 2 {
+			values = append(values, strings.TrimSpace(parts[0]))
+		}
+	}
+
+	server, err := psi_server.CreateWithNewKey(true)
+	if err != nil {
+		fmt.Errorf("Failed to create a PSI server %v", err)
+	}
+
+	// Create the setup
+	serverSetup, err := server.CreateSetupMessage(0, 1, values, psi_ds.Raw)
+	if err != nil {
+		fmt.Errorf("Failed to create serverSetup: %v", err)
+	}
+	serializedServerSetup, err := proto.Marshal(serverSetup)
+	if err != nil {
+		fmt.Errorf("Failed to serialize serverSetup: %v", err)
+	}
+
+	// Get the response
+	response, err := server.ProcessRequest(psiRequest)
+	if err != nil {
+		fmt.Errorf("Failed to process request: %v", err)
+	}
+	serializedResponse, err := proto.Marshal(response)
+	if err != nil {
+		fmt.Errorf("Failed to serialize response: %v", err)
+	}
+
+	// Send the serialized response back to the client
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("PSI-Response-Length", fmt.Sprint(len(serializedResponse)))
+	w.Header().Set("PSI-Setup-Length", fmt.Sprint(len(serializedServerSetup)))
+	w.WriteHeader(http.StatusOK) // Set the response code before writing the body
+	w.Write(serializedResponse)
+	w.Write(serializedServerSetup)
 }
