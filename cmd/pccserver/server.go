9080cd160112f2fe7ed373e54d3e0dee1ba3916c
Petr Kamnev <petrkamnevyt@gmail.com>
2024-04-22 15:44:43 +0300

    ntlm server support

--- cmd/pccserver/server.go
+++ cmd/pccserver/server.go
@@ -52,64 +52,70 @@ func initServerCmd() {
 func handleRange(w http.ResponseWriter, r *http.Request) {
 	prefix := strings.ToUpper(strings.TrimPrefix(r.URL.Path, "/range/"))
 	mode := r.URL.Query().Get("mode")
-	//addPadding := r.Header.Get("Add-Padding") == "true"
-	ifModifiedSince := r.Header.Get("If-Modified-Since")
-	if mode != "ntlm" {
-		// Construct the filename based on the given prefix
-		filename := filepath.Join(getStoragePath(), "sha1", prefix+".txt")
+	var folderPath string
 
-		// Check if the file exists
-		_, err := os.Stat(filename)
-		if os.IsNotExist(err) {
-			// If the file doesn't exist, set the response code to 400 and write the error message to the response body
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("The hash prefix was not in a valid format"))
-			return
-		}
-
-		// Open the file
-		file, err := os.Open(filename)
-		if err != nil {
-			// If there is an error opening the file, handle it (you may choose to log the error or handle it differently)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal Server Error"))
-			return
-		}
-		defer file.Close()
-
-		// Get file information
-		fileInfo, err := file.Stat()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal Server Error"))
-			return
-		}
-
-		// Check if the file creation date is later than If-Modified-Since
-		if ifModifiedSince != "" && !fileInfo.ModTime().IsZero() && !fileInfo.ModTime().Before(parseTime(ifModifiedSince)) {
-			w.WriteHeader(http.StatusNotModified)
-			return
-		}
-
-		fileSize := fileInfo.Size()
-		fileContent := make([]byte, fileSize)
-		_, err = file.Read(fileContent)
-		if err != nil {
-			// If there is an error reading the file, handle it (you may choose to log the error or handle it differently)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal Server Error"))
-			return
-		}
-
-		// Set Last-Modified header using the file creation date
-		w.Header().Set("Last-Modified", fileInfo.ModTime().UTC().Format(http.TimeFormat))
-
-		// Set the response code to 200
-		w.WriteHeader(http.StatusOK)
-
-		// Write the file content to the response body
-		w.Write(fileContent)
+	// Determine the storage subdirectory based on the mode parameter
+	if mode == "ntlm" {
+		folderPath = "ntlm"
+	} else {
+		folderPath = "sha1"
 	}
+
+	// Construct the filename based on the given prefix
+	filename := filepath.Join(getStoragePath(), folderPath, prefix+".txt")
+
+	// Check if the file exists
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		// If the file doesn't exist, set the response code to 400 and write the error message to the response body
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("The hash prefix was not in a valid format"))
+		return
+	}
+
+	// Open the file
+	file, err := os.Open(filename)
+	if err != nil {
+		// If there is an error opening the file, handle it (you may choose to log the error or handle it differently)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+	defer file.Close()
+
+	// Get file information
+	fileInfo, err := file.Stat()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+
+	// Check if the file creation date is later than If-Modified-Since
+	ifModifiedSince := r.Header.Get("If-Modified-Since")
+	if ifModifiedSince != "" && !fileInfo.ModTime().IsZero() && !fileInfo.ModTime().Before(parseTime(ifModifiedSince)) {
+		w.WriteHeader(http.StatusNotModified)
+		return
+	}
+
+	fileSize := fileInfo.Size()
+	fileContent := make([]byte, fileSize)
+	_, err = file.Read(fileContent)
+	if err != nil {
+		// If there is an error reading the file, handle it (you may choose to log the error or handle it differently)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+
+	// Set Last-Modified header using the file creation date
+	w.Header().Set("Last-Modified", fileInfo.ModTime().UTC().Format(http.TimeFormat))
+
+	// Set the response code to 200
+	w.WriteHeader(http.StatusOK)
+
+	// Write the file content to the response body
+	w.Write(fileContent)
 }
 
 func parseTime(value string) time.Time {
