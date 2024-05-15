fd7b8bdcce9cfbd13ba8b69f236aecb9d1dc56af
Petr Kamnev <petrkamnevyt@gmail.com>
2024-05-15 20:13:25 +0300

    client parameter values renamed

--- cmd/PasswordCompromiseCheckClient/main.go
+++ cmd/PasswordCompromiseCheckClient/main.go
@@ -8,19 +8,19 @@ import (
 )
 
 func main() {
-	mode := flag.String("mode", "SHA-1", "The mode of the server (\"SHA-1\", \"NTLM\", \"PSI\")")
+	mode := flag.String("mode", "sha1", "The mode of the server (\"sha1\", \"ntlm\", \"psi\")")
 	password := flag.String("password", "", "The password to check")
 	url := flag.String("url", "", "The password compromise check server url")
 	flag.Parse()
-	if *mode == "SHA-1" {
+	if *mode == "sha1" {
 		result, err := PasswordCompromiseCheckClientLib.CheckSHA1Password(*password, *url)
 		if err != nil {
 			fmt.Println("Error checking password:", err)
 			return
 		}
 		fmt.Println(result)
-	} else if *mode == "NTLM" {
-	} else if *mode == "PSI" {
+	} else if *mode == "ntlm" {
+	} else if *mode == "psi" {
 		result, err := PasswordCompromiseCheckClientLib.CheckSHA1PSIPassword(*password, *url)
 		if err != nil {
 			fmt.Println("Error checking password:", err)
